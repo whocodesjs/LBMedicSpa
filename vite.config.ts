8f5506045212738d05878a835f2f75c92136fd6f
Jovain Chisholm <whocodesjs@gmail.com>
2025-04-03 01:47:41 -0400

    UPDATE

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-  // base: process.env.VITE_BASE_PATH || "/LBMedicSpa",
+  // server: {
+  //   port: 5173,
+  //   open: true,
+  // },
+  base: process.env.VITE_BASE_PATH || "/",
 });
